fe397d558b3ddecee9145d7ebab2cf0d08f270d0
KeenenMosleyJr <keenmosley@tangistudents.org>
2026-05-01 17:24:02 +0000

    completed bouncing box project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,15 +20,25 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+         var circle;
+         var circles = [];                                               
 
 
         // TODO 2 : Create a function that draws a circle 
-        
+        var drawCircle = function() {
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2 );
+            physikz.addRandomVelocity(circle, canvas, 5.5);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
 
         // TODO 3 : Call the drawCircle() function
-
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
 
 
         // TODO 7 : Use a loop to create multiple circles
@@ -47,10 +57,18 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-
+                physikz.updatePosition();
+                physikz.updatePosition();
+                physikz.updatePosition();
+                physikz.updatePosition();
+                physikz.updatePosition();
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
-           
+               game.checkCirclePosition();
+               game.checkCirclePosition();
+               game.checkCirclePosition();
+               game.checkCirclePosition();
+               game.checkCirclePosition();
 
             // TODO 8 / TODO 9 : Iterate over the array
            
